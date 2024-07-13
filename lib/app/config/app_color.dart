abead4e217fdc7dccf9431766df142c18a129c80
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-13 20:07:05 +0900

    feat. gpt profile

--- lib/app/config/app_color.dart
+++ lib/app/config/app_color.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 Color primary = const Color(0xFF0085FF);
+Color secondary = const Color(0xFF6C6B6B);
--- lib/presentation/pages/input_user_info_screen.dart
+++ lib/presentation/pages/input_user_info_screen.dart
@@ -13,10 +13,42 @@ class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'Hello, Flutter!',
-        style: TextStyle(color: primary),
+      appBar: AppBar(
+        title: const Text(
+          '취업할 수 있을까..?',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: primary,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+          child: Column(
+            children: [
+              _buildGPTProfile(context),
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+Widget _buildGPTProfile(BuildContext context) {
+  return Center(
+    child: Column(
+      children: [
+        Image.asset('asset/images/gpt_profile.png'),
+        const SizedBox(height: 15),
+        const Text(
+          '취업운 GPT',
+          style: TextStyle(fontSize: 17),
+        ),
+        Text(
+          '#취업운 #성공운',
+          style: TextStyle(color: secondary),
+        )
+      ],
+    ),
+  );
 }
