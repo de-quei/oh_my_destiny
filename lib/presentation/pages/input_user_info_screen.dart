5e302310b47e15f9e52e324862f42a5457bcf922
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-13 20:20:49 +0900

    feat. description container

--- lib/presentation/pages/input_user_info_screen.dart
+++ lib/presentation/pages/input_user_info_screen.dart
@@ -25,7 +25,9 @@ class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
           child: Column(
             children: [
-              _buildGPTProfile(context),
+              _buildGPTProfile(context), // gpt 프로필 빌드
+              const SizedBox(height: 15),
+              _buildDescription(context), // 설명 빌드
             ],
           ),
         ),
@@ -34,6 +36,7 @@ class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
   }
 }
 
+// gpt 프로필
 Widget _buildGPTProfile(BuildContext context) {
   return Center(
     child: Column(
@@ -42,13 +45,33 @@ Widget _buildGPTProfile(BuildContext context) {
         const SizedBox(height: 15),
         const Text(
           '취업운 GPT',
-          style: TextStyle(fontSize: 17),
+          style: TextStyle(fontSize: 20),
         ),
         Text(
           '#취업운 #성공운',
           style: TextStyle(color: secondary),
         )
       ],
+    ),
+  );
+}
+
+Widget _buildDescription(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+    decoration: BoxDecoration(
+      color: primary,
+      borderRadius: const BorderRadius.only(
+        // 말풍선 모양을 위한 Radius
+        topLeft: Radius.circular(15),
+        topRight: Radius.circular(15),
+        bottomRight: Radius.circular(15),
+      ),
+    ),
+    child: const Text(
+      '안녕! 나는 실리콘밸리 gpt야!\n취업에 대한 걱정이 있는 것 같네?\n걱정하지마 내가 다 봐줄게!\n일단 아래 정보를 다 작성해줄래?',
+      style: TextStyle(color: Colors.white, fontSize: 16),
     ),
   );
 }
