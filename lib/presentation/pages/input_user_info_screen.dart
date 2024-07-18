3f4340d9458640fed0ebdad33f52190214a0825b
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-18 11:02:00 +0900

    feat. gender select button

--- lib/presentation/pages/input_user_info_screen.dart
+++ lib/presentation/pages/input_user_info_screen.dart
@@ -10,6 +10,19 @@ class InputUserInfoScreen extends StatefulWidget {
 
 class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
   final TextEditingController _usernameController = TextEditingController();
+  String? _selectedGender;
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  void _onGenderSelected(String gender) {
+    setState(() {
+      _selectedGender = gender;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +32,7 @@ class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
           '취업할 수 있을까..?',
           style: TextStyle(color: Colors.white, fontSize: 17),
         ),
-        centerTitle: true, // 안드로이드에선 centerTitle을 설정해주어야 함
+        centerTitle: true,
         backgroundColor: primary,
       ),
       body: SingleChildScrollView(
@@ -28,15 +41,20 @@ class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
           child: Column(
             children: [
               const SizedBox(height: 15),
-              const GPTProfile(), // gpt 프로필 빌드
-              const SizedBox(height: 15),
-              const InfoContainer(), // 설명 빌드
+              gptProfile(context),
+              const SizedBox(height: 20),
+              infoContainer(context),
               const SizedBox(height: 30),
-              UserNameInput(controller: _usernameController), // 이름 입력
+              UserNameInput(controller: _usernameController),
               const SizedBox(height: 20),
-              const Description(), // 개인정보 수집 용도 빌드
+              UserGenderSelection(onGenderSelected: _onGenderSelected),
               const SizedBox(height: 20),
-              SubmitButton(usernameController: _usernameController),
+              description(context),
+              const SizedBox(height: 20),
+              SubmitButton(
+                usernameController: _usernameController,
+                selectedGender: _selectedGender,
+              ),
             ],
           ),
         ),
@@ -46,95 +64,43 @@ class _InputUserInfoScreenState extends State<InputUserInfoScreen> {
 }
 
 // gpt 프로필
-class GPTProfile extends StatelessWidget {
-  const GPTProfile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Image.asset('asset/images/gpt_profile.png'),
-          const SizedBox(height: 15),
-          const Text(
-            '취업운 GPT',
-            style: TextStyle(fontSize: 20),
-          ),
-          Text(
-            '#취업운 #성공운',
-            style: TextStyle(color: secondary),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-// 설명 컨테이너
-class InfoContainer extends StatelessWidget {
-  const InfoContainer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-      decoration: BoxDecoration(
-        color: primary,
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(15),
-          topRight: Radius.circular(15),
-          bottomRight: Radius.circular(15),
+Widget gptProfile(BuildContext context) {
+  return Center(
+    child: Column(
+      children: [
+        Image.asset('asset/images/gpt_profile.png'),
+        const SizedBox(height: 15),
+        const Text(
+          '취업운 GPT',
+          style: TextStyle(fontSize: 20),
         ),
-      ),
-      child: const Text(
-        '안녕! 나는 취업운을 알려주는 gpt야!\n취업에 대한 걱정이 있는 것 같네?\n걱정하지마 내가 다 봐줄게!\n일단 아래 정보를 다 작성해줄래?',
-        style: TextStyle(color: Colors.white, fontSize: 16),
-      ),
-    );
-  }
+        Text(
+          '#취업운 #성공운',
+          style: TextStyle(color: secondary),
+        )
+      ],
+    ),
+  );
 }
 
-// 개인정보 수집 용도 설명
-class Description extends StatelessWidget {
-  const Description({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
-      decoration: BoxDecoration(
-          color: const Color(0xFFF0F0F0),
-          borderRadius: BorderRadius.circular(15)),
-      child: const Column(
-        children: [
-          Row(
-            children: [
-              Icon(
-                Icons.favorite_border_rounded,
-                color: Color(0xFF999999),
-                size: 19,
-              ),
-              SizedBox(width: 5),
-              Text(
-                '안심하세요!',
-                style: TextStyle(
-                    color: Color(0xFF999999),
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          SizedBox(height: 7),
-          Text(
-            '내 팔자야에서 수집한 개인정보는 내 팔자야 내에서 운세 서비스를 제공하기 위한 용도 이외에는 사용되지 않습니다.',
-            style: TextStyle(fontSize: 13, color: Color(0xFFA0A0A0)),
-          )
-        ],
+// 안내
+Widget infoContainer(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+    decoration: BoxDecoration(
+      color: primary,
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(15),
+        topRight: Radius.circular(15),
+        bottomRight: Radius.circular(15),
       ),
-    );
-  }
+    ),
+    child: const Text(
+      '안녕! 나는 취업운을 알려주는 gpt야!\n취업에 대한 걱정이 있는 것 같네?\n걱정하지마 내가 다 봐줄게!\n일단 아래 정보를 다 작성해줄래?',
+      style: TextStyle(color: Colors.white, fontSize: 16),
+    ),
+  );
 }
 
 // 이름 입력 필드
@@ -178,11 +144,117 @@ class UserNameInput extends StatelessWidget {
   }
 }
 
+// 성별 선택 버튼
+class UserGenderSelection extends StatefulWidget {
+  final ValueChanged<String> onGenderSelected;
+
+  const UserGenderSelection({super.key, required this.onGenderSelected});
+
+  @override
+  State<UserGenderSelection> createState() => _UserGenderSelectionState();
+}
+
+class _UserGenderSelectionState extends State<UserGenderSelection> {
+  String? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            '성별',
+            style: TextStyle(fontSize: 17),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              genderButton(context, '남성'),
+              genderButton(context, '여성'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget genderButton(BuildContext context, String gender) {
+    bool isSelected = selectedGender == gender;
+    return SizedBox(
+      width: MediaQuery.of(context).size.width / 2.3,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          padding: const EdgeInsets.all(20),
+          foregroundColor: secondary,
+          side: BorderSide(color: isSelected ? primary : stroke),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: () {
+          setState(() {
+            selectedGender = gender;
+          });
+          widget.onGenderSelected(gender);
+        },
+        child: Text(
+          gender,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+// 개인정보 활용 안내
+Widget description(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+    decoration: BoxDecoration(
+        color: const Color(0xFFF0F0F0),
+        borderRadius: BorderRadius.circular(15)),
+    child: const Column(
+      children: [
+        Row(
+          children: [
+            Icon(
+              Icons.favorite_border_rounded,
+              color: Color(0xFF999999),
+              size: 19,
+            ),
+            SizedBox(width: 5),
+            Text(
+              '안심하세요!',
+              style: TextStyle(
+                  color: Color(0xFF999999),
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        SizedBox(height: 7),
+        Text(
+          '내 팔자야에서 수집한 개인정보는 내 팔자야 내에서 운세 서비스를 제공하기 위한 용도 이외에는 사용되지 않습니다.',
+          style: TextStyle(fontSize: 13, color: Color(0xFFA0A0A0)),
+        )
+      ],
+    ),
+  );
+}
+
 // 제출 버튼
 class SubmitButton extends StatelessWidget {
   final TextEditingController usernameController;
+  final String? selectedGender;
 
-  const SubmitButton({super.key, required this.usernameController});
+  const SubmitButton({
+    super.key,
+    required this.usernameController,
+    this.selectedGender,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -198,7 +270,9 @@ class SubmitButton extends StatelessWidget {
         ),
         onPressed: () {
           final name = usernameController.text;
+          final gender = selectedGender;
           print('Name: $name');
+          print('Gender: $gender');
         },
         child: const Text(
           '사주보기',
