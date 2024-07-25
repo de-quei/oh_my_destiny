bf8b3be51cd47f1c52ba12dbe4a022518886a86e
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-07-25 16:25:54 +0900

    style. response page 스타일링

--- lib/presentation/pages/gpt_response_screen.dart
+++ lib/presentation/pages/gpt_response_screen.dart
@@ -18,18 +18,35 @@ class GptResponseScreen extends StatelessWidget {
         centerTitle: true,
         backgroundColor: primary,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (gptResponse
-                .isNotEmpty) // Display response only if it's not empty
-              Text(
-                gptResponse,
-                style: const TextStyle(fontSize: 16, color: Colors.black),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Image.asset('asset/images/gpt_profile.png'),
+              const SizedBox(height: 15),
+              const Text(
+                '취업운 GPT',
+                style: TextStyle(fontSize: 18),
               ),
-          ],
+              const SizedBox(height: 25),
+              if (gptResponse.isNotEmpty)
+                Container(
+                  padding: const EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: primary),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                        bottomRight: Radius.circular(15)),
+                  ),
+                  child: Text(
+                    gptResponse,
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+            ],
+          ),
         ),
       ),
     );
